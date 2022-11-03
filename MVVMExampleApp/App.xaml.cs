81eb1385523b831b5a194a76bc5272210ab54698
Tobias-Cargo-BI <110807150+Tobias-Cargo-BI@users.noreply.github.com>
2022-11-03 15:09:31 +0100

    Add project files.

---
+++ MVVMExampleApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVMExampleApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new SimpleMVVMView()
+            {
+                DataContext = new SimpleMVVMViewModel()
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ MVVMExampleApp/MVVMExample/Viewmodels/SimpleMVVMViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MVVMExampleApp
+{
+    public class SimpleMVVMViewModel : ObservableObject
+    {
+    }
+}
